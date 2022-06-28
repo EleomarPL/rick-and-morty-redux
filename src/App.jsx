1969a2895d7162927e436d20700a6f2cdcecb5fc
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2022-06-28 10:04:47 -0500

    initialized project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import '@fontsource/roboto'
+
+function App () {
+  return (
+    <h1>Hola Mundo con Vite</h1>
+  )
+}
+
+export default App
