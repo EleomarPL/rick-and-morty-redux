ea0a65160909dc46ac593cf49a918b308b6646bc
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2022-06-29 09:56:29 -0500

    feat: Searcher was added

---
+++ src/components/views/InputSearchWithDebounder.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types'
+import { debounce } from 'lodash'
+import { useEffect, useMemo } from 'react'
+
+import Icon from '../common/Icon'
+
+const InputSearchWithDebounder = ({ namePage, setSearcher }) => {
+  const handleChanges = (event) => {
+    setSearcher(event.target.value)
+  }
+  const handleDebouncerChanges = useMemo(() =>
+    debounce(handleChanges, 1000)
+  , [])
+
+  useEffect(() => {
+    return () => {
+      handleDebouncerChanges.cancel()
+    }
+  }, [])
+
+  return (
+    <div className="container d-flex align-items-center justify-content-center">
+      <Icon className="bi bi-search" withSpace={ true } />
+      <input
+        type="text"
+        placeholder={ `Buscar ${namePage}` }
+        className="form-control"
+        id="search"
+        onChange={ handleDebouncerChanges }
+      />
+    </div>
+  )
+}
+
+InputSearchWithDebounder.propTypes = {
+  namePage: PropTypes.string.isRequired,
+  setSearcher: PropTypes.func.isRequired
+}
+
+export default InputSearchWithDebounder
