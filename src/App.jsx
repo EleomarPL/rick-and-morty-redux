e6feeacfc974b9d65b7f655a2bcaa5fc180d92f0
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2022-06-28 21:42:14 -0500

    feat: Base colors and base browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components'
+
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
@@ -5,8 +7,16 @@ import '@fontsource/roboto'
 
 function App () {
   return (
-    <h1>Hola Mundo con Vite</h1>
+    <MainApp>
+      <h1>App</h1>
+    </MainApp>
   )
 }
+
+const MainApp = styled.div`
+  background-color: var(--bg-primary);
+  color: var(--text-primary);
+  min-height: 100vh;
+`
 
 export default App
