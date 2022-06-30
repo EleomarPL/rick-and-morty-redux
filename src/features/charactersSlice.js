784ec118815d7a4306c5ed6bcb4750c86af07cb4
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2022-06-30 07:45:57 -0500

    feat: Get characters in page characters

--- src/features/charactersSlice.js
+++ src/features/charactersSlice.js
@@ -1,14 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+import { searchCharacterByNameAxios } from '../services/apis/character'
+
+export const searchByName = createAsyncThunk(
+  'characters/searchByName',
+  async (action) => {
+    return await searchCharacterByNameAxios(action)
+  }
+)
 
 export const charactersSlice = createSlice({
   name: 'characters',
   initialState: {
-    value: []
+    value: [],
+    status: 'idle'
   },
   reducers: {
     getInitial: state => {
       state.value = []
     }
+  },
+  extraReducers (builder) {
+    builder
+      .addCase(searchByName.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(searchByName.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.value = action.payload.results
+      })
+      .addCase(searchByName.rejected, (state, action) => {
+        state.status = 'error'
+      })
   }
 })
 
--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,16 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 import InputSearchWithDebounder from '../components/views/InputSearchWithDebounder'
+import { searchByName } from '../features/charactersSlice'
 
 const Characters = () => {
+  const characters = useSelector(state => state.characters.value)
+  const status = useSelector(state => state.characters.status)
+  const dispach = useDispatch()
+
   const [searcher, setSearcher] = useState('')
+
+  useEffect(() => {
+    dispach(searchByName(searcher))
+  }, [searcher])
 
   return (
     <div className="container-fluid">
       <InputSearchWithDebounder namePage="Personaje"
         setSearcher={ setSearcher }
       />
-      <span>{ searcher }</span>
+      { status === 'loading' && <div>Cargando...</div> }
+      { characters &&
+        characters.map(character => (
+          <div key={ character.id }>
+            <h2>{ character.name }</h2>
+            <p>{ character.status }</p>
+            <p>{ character.species }</p>
+          </div>
+        ))
+      }
     </div>
   )
 }
