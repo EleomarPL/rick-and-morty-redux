fd6c048aa9acc26397a9f46ff049b27c5234d051
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2022-06-30 07:44:29 -0500

    feat: Added base api and api for get characters by name

---
+++ src/services/BASE_API.js
@@ -0,0 +1,1 @@
+export const BASE_API = import.meta.env.VITE_BASE_API
---
+++ src/services/apis/character.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+import { BASE_API } from '../BASE_API'
+
+const CHARACTER_API = `${BASE_API}/character`
+
+export const searchCharacterByNameAxios = async (name) => {
+  const response = await axios.get(`${CHARACTER_API}?name=${name}`)
+  return response.data
+}
