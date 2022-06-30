d26b27905a7cfb5ab85fa4b2dcec63576c737baf
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2022-06-30 07:54:34 -0500

    feat: Get locations in page locations

--- src/features/locationsSlice.js
+++ src/features/locationsSlice.js
@@ -1,14 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+import { searchLocationByNameAxios } from '../services/apis/locations'
+
+export const searchByName = createAsyncThunk(
+  'locations/searchByName',
+  async (action) => {
+    return await searchLocationByNameAxios(action)
+  }
+)
 
 export const locationsSlice = createSlice({
   name: 'locations',
   initialState: {
-    value: []
+    value: [],
+    status: 'idle'
   },
   reducers: {
     getInitial: state => {
       state.value = []
     }
+  },
+  extraReducers (builder) {
+    builder
+      .addCase(searchByName.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(searchByName.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.value = action.payload.results
+      })
+      .addCase(searchByName.rejected, (state, action) => {
+        state.status = 'error'
+      })
   }
 })
 
--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 import InputSearchWithDebounder from '../components/views/InputSearchWithDebounder'
+import { searchByName } from '../features/locationsSlice'
 
 const Locations = () => {
+  const locations = useSelector(state => state.locations.value)
+  const status = useSelector(state => state.locations.status)
+  const dispatch = useDispatch()
+
   const [searcher, setSearcher] = useState('')
+
+  useEffect(() => {
+    dispatch(searchByName(searcher))
+  }, [searcher])
 
   return (
     <div className="container-fluid">
       <InputSearchWithDebounder namePage="Locacione"
         setSearcher={ setSearcher }
       />
-      <span>{ searcher }</span>
+      { status === 'loading' && <div>Cargando...</div> }
+      { locations &&
+        locations.map(location => (
+          <div key={ location.id }>
+            <h2>{ location.name }</h2>
+          </div>
+        ))
+      }
     </div>
   )
 }
