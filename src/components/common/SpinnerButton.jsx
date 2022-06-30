a34b62193f05f64785e2683723f2b5b250de35a0
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2022-06-30 11:15:21 -0500

    feat: Spinner to loading button

---
+++ src/components/common/SpinnerButton.jsx
@@ -0,0 +1,10 @@
+const SpinnerButton = () => {
+  return (
+    <span className="spinner-border spinner-border-sm" role="status"
+      aria-hidden="true" style={ { marginRight: '5px' } }
+    >
+    </span>
+  )
+}
+
+export default SpinnerButton
