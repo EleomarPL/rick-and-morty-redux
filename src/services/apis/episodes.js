e9632b5663e1b217b7bfc636416438daa546fc70
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2022-06-30 07:53:43 -0500

    feat: Api for locations and episodes was added

---
+++ src/services/apis/episodes.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+import { BASE_API } from '../BASE_API'
+
+const EPISODE_API = `${BASE_API}/episode`
+
+export const searchEpisodeByNameAxios = async (name) => {
+  const response = await axios.get(`${EPISODE_API}?name=${name}`)
+  return response.data
+}
---
+++ src/services/apis/locations.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+import { BASE_API } from '../BASE_API'
+
+const LOCATION_API = `${BASE_API}/location`
+
+export const searchLocationByNameAxios = async (name) => {
+  const response = await axios.get(`${LOCATION_API}?name=${name}`)
+  return response.data
+}
