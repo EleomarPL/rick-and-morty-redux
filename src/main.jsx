34647b43f8bdfa89d6ff56bcb5e258ca0b73ed65
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2022-06-30 07:43:55 -0500

    fix: remove Strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
@@ -9,11 +8,9 @@ import { store } from './store'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={ store }>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>
+  <Provider store={ store }>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 )
