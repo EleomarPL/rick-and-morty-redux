b03a9b8593e3e98408bc596e355e8d4c4291a3ad
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2022-06-30 07:54:21 -0500

    feat: Get episodes in page episodes

--- src/features/episodesSlice.js
+++ src/features/episodesSlice.js
@@ -1,14 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+import { searchEpisodeByNameAxios } from '../services/apis/episodes'
+
+export const searchByName = createAsyncThunk(
+  'episodes/searchByName',
+  async (action) => {
+    return await searchEpisodeByNameAxios(action)
+  }
+)
 
 export const episodesSlice = createSlice({
   name: 'episodes',
   initialState: {
-    value: []
+    value: [],
+    status: 'idle'
   },
   reducers: {
     getInitial: state => {
       state.value = []
     }
+  },
+  extraReducers (builder) {
+    builder
+      .addCase(searchByName.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(searchByName.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.value = action.payload.results
+      })
+      .addCase(searchByName.rejected, (state, action) => {
+        state.status = 'error'
+      })
   }
 })
 
--- src/pages/Episodes.jsx
+++ src/pages/Episodes.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 import InputSearchWithDebounder from '../components/views/InputSearchWithDebounder'
+import { searchByName } from '../features/episodesSlice'
 
 const Episodes = () => {
+  const episodes = useSelector(state => state.episodes.value)
+  const status = useSelector(state => state.episodes.status)
+  const dispatch = useDispatch()
+
   const [searcher, setSearcher] = useState('')
+
+  useEffect(() => {
+    dispatch(searchByName(searcher))
+  }, [searcher])
 
   return (
     <div className="container-fluid">
       <InputSearchWithDebounder namePage="Episodio"
         setSearcher={ setSearcher }
       />
-      <span>{ searcher }</span>
+      { status === 'loading' && <div>Cargando...</div> }
+      { episodes &&
+        episodes.map(episode => (
+          <div key={ episode.id }>
+            <h2>{ episode.name }</h2>
+          </div>
+        ))
+      }
     </div>
   )
 }
